7e9bc1e56131cb7c08558e26743c3ea54e67d445
david bess <bessdavid93@gmail.com>
2020-10-05 13:59:50 -0500

    post exercise

--- src/main/java/com/codeup/bloggers/HelloController.java
+++ src/main/java/com/codeup/bloggers/HelloController.java
@@ -1,4 +1,4 @@
-package com.codeup.bloggers.controllers;
+package com.codeup.bloggers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/codeup/bloggers/HomeController.java
+++ src/main/java/com/codeup/bloggers/HomeController.java
@@ -1,12 +1,14 @@
-package com.codeup.bloggers.controllers;
+package com.codeup.bloggers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
+    @ResponseBody
     public String homePage() {
-        return "home";
+        return "This is my home page!";
     }
 }
